c8a2db2f3a4c8b9202c3118dc269c564e4fd1be0
RuchikaRaj <ruchika.raj@red-gate.com>
2012-12-07 15:24:01 +0000

    Demo-2 #comment Sycn comment

--- TestProject/ClassLibrary1/ClassLibrary1/Class1.cs
+++ TestProject/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ClassLibrary1
+namespace AgethingyTestLib
 {
     public class Class1
     {
+        public void DisplayMessage()
+        {
+            Console.WriteLine("Test String");
+        }
     }
 }
